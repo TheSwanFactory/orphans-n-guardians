043f4b712dd0a58a159b9bbb4db9612b8aef1151
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2024-11-30 23:05:12 -0800

    fmt

--- islands/Greet.tsx
+++ islands/Greet.tsx
@@ -2,31 +2,31 @@ import type { Signal } from "@preact/signals";
 import { useState } from "preact/hooks";
 
 interface GreetProps {
-    text: Signal<string>;
+  text: Signal<string>;
 }
 
 export default function Greet(props: GreetProps) {
-    const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState("");
 
-    const handleSubmit = (event: Event) => {
-        event.preventDefault();
-        globalThis.location.href = `/greet/${inputValue}`;
-    };
+  const handleSubmit = (event: Event) => {
+    event.preventDefault();
+    globalThis.location.href = `/greet/${inputValue}`;
+  };
 
-    return (
-        <div class="flex gap-8 py-6">
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    value={inputValue}
-                    onInput={(e) => setInputValue((e.target as HTMLInputElement).value)}
-                    class="border p-2"
-                />
-                <button type="submit" class="ml-2 p-2 bg-blue-500 text-white">
-                    Submit
-                </button>
-            </form>
-            <p class="text-3xl tabular-nums">{props.text}</p>
-        </div>
-    );
+  return (
+    <div class="flex gap-8 py-6">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={inputValue}
+          onInput={(e) => setInputValue((e.target as HTMLInputElement).value)}
+          class="border p-2"
+        />
+        <button type="submit" class="ml-2 p-2 bg-blue-500 text-white">
+          Submit
+        </button>
+      </form>
+      <p class="text-3xl tabular-nums">{props.text}</p>
+    </div>
+  );
 }
--- islands/Joke.tsx
+++ islands/Joke.tsx
@@ -8,16 +8,16 @@ interface JokeProps {
 export default function Joke(props: JokeProps) {
   return (
     <div class="flex gap-8 py-6">
-    <Button
-      onClick={async () => {
-          const response = await fetch('/api/joke');
+      <Button
+        onClick={async () => {
+          const response = await fetch("/api/joke");
           const data = await response.text();
           props.text.value = data;
-      }}
-    >
-      Tell me a joke
-    </Button>
-    <p class="text-3xl tabular-nums">{props.text}</p>
+        }}
+      >
+        Tell me a joke
+      </Button>
+      <p class="text-3xl tabular-nums">{props.text}</p>
     </div>
   );
 }
