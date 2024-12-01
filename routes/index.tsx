dc9dad8aef67aaf06269c6b7d4da4a0d9bf3728c
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2024-11-30 22:28:34 -0800

    add kickstarter

--- routes/index.tsx
+++ routes/index.tsx
@@ -13,12 +13,25 @@ export default function Home() {
           height="128"
           alt="the Orphans & Guardians logo: gold+steel winged shield"
         />
-        <h1 class="text-4xl font-bold">Welcome to Orphans & Gaurdians</h1>
+        <h1 class="text-4xl font-bold">Welcome to Orphans & Guardians</h1>
         <p class="my-4">
           Try updating this message in the
           <code class="mx-2">./routes/index.tsx</code> file, and refresh.
         </p>
         <Counter count={count} />
+      </div>
+      <div class="flex flex-col items-center">
+        <h2 class="text-2xl font-bold">Check out our Kickstarter!</h2>
+        <a
+          href="https://ihack.us/2024/11/29/kickstarter-pitch-orphans-guardians-the-quest-for-your-true-self/"
+          class="mt-4 w-1/2"
+        >
+          <img
+            src="/kickstarter.jpg"
+            alt="Check out our Kickstarter"
+            class="w-full h-auto"
+          />
+        </a>
       </div>
     </div>
   );
