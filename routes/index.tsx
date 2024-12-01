4e589ec76239a4cc5378f81ffd8485215fb537d3
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2024-11-30 20:51:13 -0800

    update logo

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,16 +4,16 @@ import Counter from "../islands/Counter.tsx";
 export default function Home() {
   const count = useSignal(3);
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
+    <div class="px-4 py-8 mx-auto bg-[#86d6ef]">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <img
           class="my-6"
-          src="/logo.svg"
+          src="/ong-logo.jpg"
           width="128"
           height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
+          alt="the Orphans & Guardians logo: gold+steel winged shield"
         />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
+        <h1 class="text-4xl font-bold">Welcome to Orphans & Gaurdians</h1>
         <p class="my-4">
           Try updating this message in the
           <code class="mx-2">./routes/index.tsx</code> file, and refresh.
