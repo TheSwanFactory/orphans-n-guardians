a6ea30197725c4bba2e66f4923020c07f83f7e76
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2024-12-01 21:31:17 -0800

    Storage class

---
+++ data/storage.test.ts
@@ -0,0 +1,48 @@
+import Storage from "./storage.ts";
+import { default_care } from "./defaults.ts";
+import { describe, it } from "@std/testing/bdd";
+import { assertEquals } from "@std/assert";
+
+describe("Storage", () => {
+  let storage: Storage;
+
+  it("store_value", async () => {
+    storage = new Storage("store_value", true);
+    try {
+      await storage.store_value("test", "value");
+      const value = await storage.retrieve_value("test");
+      assertEquals(value, "value", "Test failed: value should be 'value'");
+    } finally {
+      storage.dispose();
+    }
+  });
+
+  it("list_values", async () => {
+    storage = new Storage("list_values", true);
+    try {
+      await storage.store_value("test", "value");
+      await storage.store_value("tests", "values");
+
+      const values = await storage.list_values();
+      assertEquals(values.length, 2, "Test failed: there should be 2 values");
+      assertEquals(values[0], "test", "Test failed: value should be 'test'");
+      assertEquals(values[1], "tests", "Test failed: value should be 'tests'");
+    } finally {
+      storage.dispose();
+    }
+  });
+
+  it("ensure_game", async () => {
+    try {
+      storage = new Storage("ensure_game", true);
+      const game = await storage.ensure_game("test_user");
+      assertEquals(
+        JSON.stringify(game),
+        JSON.stringify(default_care),
+        "Test failed: game should be default_care",
+      );
+    } finally {
+      storage.dispose();
+    }
+  });
+});
--- data/storage.ts
+++ data/storage.ts
@@ -1,29 +1,76 @@
-import { default_prefix } from "./defaults.ts";
+import { default_care, default_prefix } from "./defaults.ts";
 
-export async function store_value(key: string, value: string): Promise<void> {
-  const kv = await Deno.openKv();
-  await kv.set([default_prefix, key], value);
-  kv.close();
-}
+class Storage {
+  private _kv: Deno.Kv | null = null;
+  private prefix: string;
+  private is_test: boolean;
 
-export function retrieve_value(key: string): Promise<string | undefined> {
-  return Deno.openKv().then(async (kv) => {
-    try {
-      const result = await kv.get([default_prefix, key]);
-      return result?.value as string | undefined;
-    } finally {
-      kv.close();
+  constructor(prefix: string = default_prefix, is_test: boolean = false) {
+    this.prefix = prefix;
+    this.is_test = is_test;
+    if (is_test) {
+      Deno.openKv(":memory:").then((kv) => {
+        this._kv = kv;
+      });
     }
-  });
+  }
+
+  private get kv(): Promise<Deno.Kv> {
+    if (!this._kv) {
+      return Deno.openKv().then((kv) => {
+        this._kv = kv;
+        return kv;
+      });
+    }
+    return Promise.resolve(this._kv);
+  }
+
+  async store_value(key: string, value: string): Promise<void> {
+    const kv = await this.kv;
+    await kv.set([this.prefix, key], value);
+  }
+
+  async retrieve_value(key: string): Promise<string | undefined> {
+    const kv = await this.kv;
+    const result = await kv.get([this.prefix, key]);
+    return result?.value as string | undefined;
+  }
+
+  async list_values(): Promise<string[]> {
+    const kv = await this.kv;
+    const entries = [];
+    for await (const entry of kv.list({ prefix: [this.prefix] })) {
+      entries.push(entry);
+    }
+    const keys = entries.map((entry) => entry.key[1] as string);
+    return keys;
+  }
+
+  async ensure_game(username: string): Promise<object> {
+    const kv = await this.kv;
+    const game = await kv.get([this.prefix, username]);
+    let value = game?.value;
+    if (!value) {
+      value = default_care;
+      await kv.set([this.prefix, username], value);
+    }
+    return value as object;
+  }
+
+  async dispose(): Promise<void> {
+    if (this.is_test) {
+      // delete all keys
+      const kv = await this.kv;
+      for await (const entry of kv.list({ prefix: [this.prefix] })) {
+        console.log("deleting", entry.key);
+        await kv.delete(entry.key);
+      }
+    }
+    if (this._kv) {
+      await this._kv.close();
+      this._kv = null;
+    }
+  }
 }
 
-export async function list_values(): Promise<string[]> {
-  const kv = await Deno.openKv();
-  const entries = [];
-  for await (const entry of kv.list({ prefix: [default_prefix] })) {
-    entries.push(entry);
-  }
-  const keys = entries.map((entry) => entry.key[1] as string);
-  kv.close();
-  return keys;
-}
+export default Storage;
