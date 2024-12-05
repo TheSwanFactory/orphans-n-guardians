36a481f6fc7f1adb58df5cbc1d99f9032d53b0d3
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2024-12-04 16:11:41 -0800

    add handler for Game

--- routes/game/[name].tsx
+++ routes/game/[name].tsx
@@ -1,12 +1,35 @@
-import { PageProps } from "$fresh/server.ts";
+import { PageProps, RouteContext } from "$fresh/server.ts";
+import Storage from "../../data/Storage.ts";
+import { Handlers } from "$fresh/server.ts";
 
-export default function Game(props: PageProps) {
+async function getGame(name: string): Promise<object> {
+  const storage = new Storage("game", false);
+  return await storage.ensure_game(name);
+}
+
+export const handler: Handlers<RouteContext> = {
+  async GET(_req, ctx) {
+    const name: string = ctx.params.name;
+    const game: object = await getGame(name);
+    ctx.params.name = name;
+    ctx.params.game = JSON.stringify(game);
+    const resp = await ctx.render();
+    resp.headers.set("X-Custom-Header", "Hello");
+    return resp;
+  },
+};
+
+export default function GameForName(props: PageProps) {
   return (
     <div>
       <head>
         <title>Game - Orphans & Guardians</title>
       </head>
       <div>Hello {props.params.name}</div>
+      <div>
+        <h2>Display Game</h2>
+        <pre>{props.params.game}</pre>
+      </div>
     </div>
   );
 }
