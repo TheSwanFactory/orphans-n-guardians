7a584d8c7aa9012663da6866756638936453be3f
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2024-12-04 16:21:54 -0800

    props.text.value

--- islands/Game.tsx
+++ islands/Game.tsx
@@ -7,11 +7,9 @@ interface GameProps {
 }
 
 export default function Game(props: GameProps) {
-  const [inputValue, setInputValue] = useState("");
-
   const handleSubmit = (event: Event) => {
     event.preventDefault();
-    globalThis.location.href = `/game/${inputValue}`;
+    globalThis.location.href = `/game/${props.text.value}`;
   };
 
   return (
@@ -19,15 +17,16 @@ export default function Game(props: GameProps) {
       <form onSubmit={handleSubmit}>
         <input
           type="text"
-          value={inputValue}
-          onInput={(e) => setInputValue((e.target as HTMLInputElement).value)}
+          value={props.text.value}
+          onInput={(e) =>
+            props.text.value = (e.target as HTMLInputElement).value}
           class="border p-2"
         />
         <button type="submit" class="ml-2 p-2 bg-blue-500 text-white">
           Submit
         </button>
       </form>
-      <p class="text-3xl tabular-nums">Example: {props.text}</p>
+      <p class="text-3xl tabular-nums">Example: {props.text.value}</p>
     </div>
   );
 }
